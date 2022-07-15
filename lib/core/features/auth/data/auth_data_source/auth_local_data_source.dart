ea0406d3fa1fb2e61829d25b71cc72d1820dfb75
Nidal <29513372+Nidal-Bakir@users.noreply.github.com>
2022-07-15 14:37:24 +0300

    update the docs for auth_local_datasource

--- lib/core/features/auth/data/auth_data_source/auth_local_data_source.dart
+++ lib/core/features/auth/data/auth_data_source/auth_local_data_source.dart
@@ -4,8 +4,8 @@ import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
 abstract class AuthLocalDataSource {
   /// Get the current local user info and data.
   /// 
-  ///  Returns null in case no (login | signUp) preformed or
-  /// the user logout.
+  /// Returns null in case no (login, signUp or loginAnonymously) preformed.
+  /// Or the user logout and did not login again as anonymous user for any error.
   Future<User?> getCurrentLoggedUser();
 }
 
