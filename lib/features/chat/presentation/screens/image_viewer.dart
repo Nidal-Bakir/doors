9216b23aa1693c50a23d3ddb841808c8b8d046b7
nidal bakir <nedalbaker2000@gmail.com>
2022-07-24 06:35:33 +0300

    hide the Status bar when opening image in fullscreen mode from chat message

--- lib/features/chat/presentation/screens/image_viewer.dart
+++ lib/features/chat/presentation/screens/image_viewer.dart
@@ -2,19 +2,48 @@ import 'dart:io';
 
 import 'package:doors/core/widgets/no_image_provided.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 
-class ImageViewer extends StatelessWidget {
+class ImageViewer extends StatefulWidget {
   static const routeName = '/image-viewer';
   final File imageFile;
   const ImageViewer({Key? key, required this.imageFile}) : super(key: key);
+
+  @override
+  State<ImageViewer> createState() => _ImageViewerState();
+}
+
+class _ImageViewerState extends State<ImageViewer> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [
+        SystemUiOverlay.bottom,
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.manual,
+      overlays: [
+        SystemUiOverlay.bottom,
+        SystemUiOverlay.top,
+      ],
+    );
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _theme = Theme.of(context);
     return PhotoView(
       imageProvider: FileImage(
-        imageFile,
+        widget.imageFile,
       ),
       loadingBuilder: (context, loadingProgress) {
         if (loadingProgress == null) {
@@ -37,7 +66,7 @@ class ImageViewer extends StatelessWidget {
         );
       },
       heroAttributes: PhotoViewHeroAttributes(
-        tag: imageFile.path,
+        tag: widget.imageFile.path,
       ),
     );
   }
